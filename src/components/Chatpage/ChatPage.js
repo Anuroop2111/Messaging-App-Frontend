6105122747e6359ded89d19ff808dbcf4793f488
Anuroop2111 <anuroopls2001@gmail.com>
2023-08-11 21:08:36 +0530

    commit

---
+++ src/components/Chatpage/ChatPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ChatPage.css';
+
+const ChatPage = () => {
+  return (
+    <div className="chat-page">
+      <div className="left-column">
+        <h2>Chats</h2>
+      </div>
+    </div>
+  );
+};
+
+export default ChatPage;
---
+++ src/components/MessagePage/MessagePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './MessagePage.css';
+
+const MsgPage = () =>{
+    return(
+        <div className="message-page">
+            <div className="right-column">
+                <h2>Messages</h2>
+            </div>
+        </div>
+    )
+}
+
+export default MsgPage;
