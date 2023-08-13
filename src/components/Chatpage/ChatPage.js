e8e8c71d88597f0f2f71f08bd54a8df97c616f3e
Anuroop2111 <anuroopls2001@gmail.com>
2023-08-13 11:53:15 +0530

    Set up basic UI

--- src/components/Chatpage/ChatPage.js
+++ src/components/Chatpage/ChatPage.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './ChatPage.css';
 
 const ChatPage = () => {
+  const [chats,setChats] = useState([]);
+  async function fetchChatNames() {
+    try {
+      const response = await fetch('http://localhost:8080/api/getChatNames/anuroop@test.com');
+      const data = await response.json();
+      console.log(data)
+      setChats(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
+  useEffect(()=>{
+  (async ()=>{
+    await fetchChatNames();
+    })()
+
+  },[])
   return (
     <div className="chat-page">
       <div className="left-column">
         <h2>Chats</h2>
+        {chats.length == 0 ?<p>No chats</p>:<div>
+          {chats.map((chat)=>(
+            <a href={`/chat/${chat.chatId}`} className="chat-list-elem" key={chat.chatId}>{chat.friendName}</a>
+          ))}
+          </div>}
       </div>
     </div>
   );
--- src/components/MessagePage/MessagePage.js
+++ src/components/MessagePage/MessagePage.js
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './MessagePage.css';
 
+
 const MsgPage = () =>{
+    const [messages,setMessages] = useState([]);
+    async function fetchMessageNames() {
+      try {
+        const response = await fetch(`http://localhost:8080/api/getMessage/w1x2y3z4`);
+        let data = await response.json();
+        data.sort()
+        setMessages(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+    useEffect(()=>{
+    (async ()=>{
+      await fetchMessageNames();
+      })()
+  
+    },[])
     return(
         <div className="message-page">
             <div className="right-column">
                 <h2>Messages</h2>
-            </div>
+                <div className="flex justify w-100">
+                {messages.length == 0?<p>No messages to show</p>:<div className="w-100 flex flex-col justify">
+                    {messages.map(msg=>(
+                        <div className={`${msg.senderId == "a1b2c3d4" ? "left-align":"right-align"}`} key={msg.timestamp}>
+                        <p >{msg.content}</p>
+                        </div>
+                    ))}
+                    </div>} 
+                </div>
+        </div>
         </div>
     )
 }
